364ebb1c155015995cd0a1e61e693d7857c4527c
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-07-09 23:14:38 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -9,8 +9,7 @@ def extract(word):
   str_wordlist=word.split(",")
   word_split=list(map(int, str_wordlist))
   for ele in word_split:
-    if ele > 1000:
-        word_split[word_split.index(ele)]=0
+    word_split[word_split.index(ele)]={True: 0, False: ele} [ele>1000]
   t_sum=sum(word_split)
   return t_sum
   
