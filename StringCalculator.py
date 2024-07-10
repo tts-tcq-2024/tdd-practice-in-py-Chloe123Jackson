ffddb8606f79350020f6d2e8bdf29361319750e8
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-07-10 23:01:37 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -6,29 +6,54 @@ def add (word):
     return extract(word)
 
 def extract(word):
-  regex = re.compile('//.\n')
-  matches = re.findall(regex, word)
-  if matches!=[]:
+  regex1 = re.compile('//.\n')
+  regex2 = re.compile(',.,')
+  regex3 = re.compile('./n.')
+  matches1 = re.findall(regex1, word)
+  matches2 = re.findall(regex2, word)
+  matches3 = re.findall(regex3, word)
+
+  if matches1!=[]:
     delimiter=matches[0][2]
     word=word.replace(matches[0],'')
-    str_wordlist=word.split(",")
+    str_wordlist=word.split(delimiter)
     word_split=list(map(int, str_wordlist))
     for ele in word_split:
       word_split[word_split.index(ele)]={True: 0, False: ele} [ele>1000]
       t_sum=sum(word_split)
       return t_sum
-  else:
-    regex=re.compile('.,.')
-    matches = re.findall(regex, word)
-    if matches!=[]:
-      delimiter=matches[0][2]
-      word=word.replace(matches[0],'')
-      str_wordlist=word.split(",")
-      word_split=list(map(int, str_wordlist))
-      for ele in word_split:
-        word_split[word_split.index(ele)]={True: 0, False: ele} [ele>1000]
-        t_sum=sum(word_split)
+  if matches2!=[]:
+    str_wordlist=word.split(',')
+    word_split=list(map(int, str_wordlist))
+    for ele in word_split:
+      word_split[word_split.index(ele)]={True: 0, False: ele} [ele>1000]
+      t_sum=sum(word_split)
       return t_sum
+  if matches3!=[]:
+    word=word.replace(\n,'')
+    str_wordlist=word.split(',')
+    word_split=list(map(int, str_wordlist))
+    for ele in word_split:
+      word_split[word_split.index(ele)]={True: 0, False: ele} [ele>1000]
+      t_sum=sum(word_split)
+      return t_sum
+  if matches1==[] and matches2==[] and matches3==[]:
+    str_wordlist=word.split(',')
+    word_split=list(map(int, str_wordlist))
+    for ele in word_split:
+      word_split[word_split.index(ele)]={True: 0, False: ele} [ele>1000]
+      t_sum=sum(word_split)
+      return t_sum
+  
+
+# def check_delimiter(word):
+#   regex1 = re.compile('//.\n')
+#   regex2=  re.compile('.,.')
+#   matches_custom=re.findall(regex1, word)
+#   matches_normal=re.findall(regex2, word)
+#   if matches_custom!=[]:
+#     delimiter=matches_custom[0][2]
+#     word=word.replace(matches_custom[0],'')
     
   
   
