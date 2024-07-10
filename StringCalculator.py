96ee5913785205236cabc20842aba2092f67ab51
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-07-10 22:38:51 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,4 +1,4 @@
-
+import re
 def add (word):
   if word == "":
     return 0
@@ -6,12 +6,30 @@ def add (word):
     return extract(word)
 
 def extract(word):
-  str_wordlist=word.split(",")
-  word_split=list(map(int, str_wordlist))
-  for ele in word_split:
-    word_split[word_split.index(ele)]={True: 0, False: ele} [ele>1000]
-  t_sum=sum(word_split)
-  return t_sum
+  regex = re.compile('//.\n')
+  matches = re.findall(regex, word)
+  if matches!=[]:
+    delimiter=matches[0][2]
+    word=word.replace(matches[0],'')
+    str_wordlist=word.split(",")
+    word_split=list(map(int, str_wordlist))
+    for ele in word_split:
+      word_split[word_split.index(ele)]={True: 0, False: ele} [ele>1000]
+      t_sum=sum(word_split)
+      return t_sum
+  else:
+    regex=re.compile('.,.')
+    matches = re.findall(regex, word)
+    if matches!=[]:
+      delimiter=matches[0][2]
+      word=word.replace(matches[0],'')
+      str_wordlist=word.split(",")
+      word_split=list(map(int, str_wordlist))
+      for ele in word_split:
+        word_split[word_split.index(ele)]={True: 0, False: ele} [ele>1000]
+        t_sum=sum(word_split)
+      return t_sum
+    
   
   
   
