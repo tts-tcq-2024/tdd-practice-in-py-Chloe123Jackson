da674743f61be21d62cab7df83d7833f0fe65ae6
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-07-10 23:48:45 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -6,54 +6,28 @@ def add (word):
     return extract(word)
 
 def extract(word):
+  
+  word,delimiter=check_delimiter(word)
+  str_wordlist=word.split(delimiter)
+  word_split=list(map(int, str_wordlist))
+  for ele in word_split:
+    word_split[word_split.index(ele)]={True: 0, False: ele} [ele>1000]
+  t_sum=sum(word_split)
+  return t_sum  
+
+def check_delimiter(word):
   regex1 = re.compile('//.\n')
-  regex2 = re.compile('.,.')
-  regex3 = re.compile('./n.')
+  regex2 = re.compile('./n.')
   matches1 = re.findall(regex1, word)
   matches2 = re.findall(regex2, word)
-  matches3 = re.findall(regex3, word)
-
+  delimiter=','
+  
   if matches1!=[]:
     delimiter=matches1[0][2]
     word=word.replace(matches1[0],'')
-    str_wordlist=word.split(delimiter)
-    word_split=list(map(int, str_wordlist))
-    for ele in word_split:
-      word_split[word_split.index(ele)]={True: 0, False: ele} [ele>1000]
-    t_sum=sum(word_split)
-    return t_sum
   if matches2!=[]:
-    str_wordlist=word.split(',')
-    word_split=list(map(int, str_wordlist))
-    for ele in word_split:
-      word_split[word_split.index(ele)]={True: 0, False: ele} [ele>1000]
-    t_sum=sum(word_split)
-    return t_sum
-  if matches3!=[]:
     word=word.replace('\n','')
-    str_wordlist=word.split(',')
-    word_split=list(map(int, str_wordlist))
-    for ele in word_split:
-      word_split[word_split.index(ele)]={True: 0, False: ele} [ele>1000]
-    t_sum=sum(word_split)
-    return t_sum
-  if matches1==[] and matches2==[] and matches3==[]:
-    str_wordlist=word.split(',')
-    word_split=list(map(int, str_wordlist))
-    for ele in word_split:
-      word_split[word_split.index(ele)]={True: 0, False: ele} [ele>1000]
-    t_sum=sum(word_split)
-    return t_sum
-  
-
-# def check_delimiter(word):
-#   regex1 = re.compile('//.\n')
-#   regex2=  re.compile('.,.')
-#   matches_custom=re.findall(regex1, word)
-#   matches_normal=re.findall(regex2, word)
-#   if matches_custom!=[]:
-#     delimiter=matches_custom[0][2]
-#     word=word.replace(matches_custom[0],'')
+   return word,delimiter
     
   
   
